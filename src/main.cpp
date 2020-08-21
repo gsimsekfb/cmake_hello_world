2125d9a103fb542f4214d11d4c165e70729937ea
Gokhan Simsek <gokhansimsekfb@gmail.com>
2020-08-21 16:25:53 +0300

    macOS: First working scan-build

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,16 @@ public:
 struct Foo {
     Foo() {};  
     ~Foo() {}; 
-    int _count; /// warning C26495: Variable 'Foo::_count' is uninitialized.     
+    int _count; /// VS: warning C26495: Variable 'Foo::_count' is uninitialized.     
 };
+
+int test2(int x){
+  return 5/(x-x); // scan-build: warning: Division by zerowarn
+}
 
 int main(int argc, char *argv[])
 {
-    /// warning C26444: Avoid unnamed objects with custom construction and destruction
+    /// VS: warning C26444: Avoid unnamed objects with custom construction and destruction
 	// Sol.1: #pragma
     #if _MSC_VER
 	#pragma warning( push )
@@ -25,7 +29,7 @@ int main(int argc, char *argv[])
 	#pragma warning( pop )
     #endif
     
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World" << std::endl;
 
 	return 0;
 }
