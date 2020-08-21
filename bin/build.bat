e5701d84e44894584babb8292afba44eb20b4e16
Gökhan Şimşek <gokhansimsekfb@gmail.com>
2020-08-21 13:04:52 +0300

    Windows: First working VS run code analysis

--- bin/build.bat
+++ bin/build.bat
@@ -1,9 +1,33 @@
-
 @echo off
 setlocal enableextensions enabledelayedexpansion
+set "param1=%~1"
 
-rem ...
+rem 0...
+echo Hint: 
+echo   build   : Normal build
+echo   build sa: Static analysis build
+echo -------------------------------
 cd ..
+
+rem 1. Del build directory
+rmdir /s /q "build"
+
+rem 2. Static analysis
+if "!param1!" == "sa" (
+  echo Static analysis in progress...
+  echo -------------------------------
+  mkdir build && cd build	
+  cmake ..
+  rem // Working
+  cmake --build . -- "/p:RunCodeAnalysis=true" "/p:CodeAnalysisRuleSet=NativeRecommendedRules.ruleset"
+  rem cmake --build . -- "/p:RunCodeAnalysis=true" "/p:CodeAnalysisRuleSet=AllRules.ruleset"
+  exit /B
+)
+
+rem 3. Normal Build
+echo Build in progress...
+echo -------------------------------
 mkdir build && cd build	
 cmake ..
 cmake --build .
+exit /B
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
-	#include<iostream>
+#include<iostream>
 
-	int main(int argc, char *argv[]){
-	   std::cout << "Hello World!" << std::endl;
-	   return 0;
-	}
+class Obj
+{
+    int i;
+public:
+    Obj(int j) : i(j) {}
+    ~Obj(){}   
+};
+
+struct Foo {
+    Foo() {};  
+    ~Foo() {}; 
+    int _count; /// warning C26495: Variable 'Foo::_count' is uninitialized.     
+};
+
+int main(int argc, char *argv[])
+{
+    /// warning C26444: Avoid unnamed objects with custom construction and destruction
+	// Sol.1: #pragma
+    #if _MSC_VER
+	#pragma warning( push )
+	// #pragma warning( disable : 26444)
+	Obj(2);	// <-- !! 	// Sol.2: (void) obj(2)
+	#pragma warning( pop )
+    #endif
+    
+    std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
