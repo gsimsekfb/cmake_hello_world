8dae731325586bc29c1a3120422d7748f02b18d4
Gökhan Şimşek <gokhansimsekfb@gmail.com>
2020-08-22 11:25:02 +0300

    Minor refactoring

--- bin/build.bat
+++ bin/build.bat
@@ -9,15 +9,17 @@ echo   build sa: Static analysis build
 echo -------------------------------
 cd ..
 
-rem 1. Del build directory
+rem 0. Del build directory
 rmdir /s /q "build"
+
+rem 1. configure
+mkdir build && cd build	
+cmake ..
 
 rem 2. Static analysis
 if "!param1!" == "sa" (
   echo Static analysis in progress...
   echo -------------------------------
-  mkdir build && cd build	
-  cmake ..
   rem // Working
   cmake --build . -- "/p:RunCodeAnalysis=true" "/p:CodeAnalysisRuleSet=NativeRecommendedRules.ruleset"
   rem cmake --build . -- "/p:RunCodeAnalysis=true" "/p:CodeAnalysisRuleSet=AllRules.ruleset"
@@ -27,7 +29,5 @@ if "!param1!" == "sa" (
 rem 3. Normal Build
 echo Build in progress...
 echo -------------------------------
-mkdir build && cd build	
-cmake ..
 cmake --build .
 exit /B
